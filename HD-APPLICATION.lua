849bfe225fcce2b32308c638f28c373e9bd3cf50
kai6969489 <aisyzahin2705@gmail.com>
2026-01-03 20:01:30 +0800

    Update HD-APPLICATION.lua

--- HD-APPLICATION.lua
+++ HD-APPLICATION.lua
@@ -1,65 +1,34 @@
--- Hi! I'm submitting a building mechanic like Build A Boat , an inventory system with data saving of the items and a shop system.
-
--- ServerScriptService --> StartServer
---// Services
-local ServerScriptService = game:GetService("ServerScriptService")
-local ReplicatedStorage = game:GetService("ReplicatedStorage")
-local Players = game:GetService("Players")
-
---//Systems
-local PlotManager = require(ServerScriptService.SystemsServer.BuildingSystem.PlotManager)
-local Purchase = require(ServerScriptService.SystemsServer.ShopSystem.Purchase)
-local Inventory = require(ServerScriptService.SystemsServer.InventorySystem.InventoryServer)
-
---// Packages
-local Packets = require(ReplicatedStorage.Shared.Packages.Packets)
-
--- signals 
-
--- give players money at the start of the game ( not data saved )
-local function OnPlayerAdded(plr : Player)
-	local money = Instance.new("IntValue")
-	money.Value = 10000
-	money.Name = "Money"
-	money.Parent = plr
-	
-	plr:SetAttribute("Launched" , false)
-end
-
-Packets.tryPurchase.OnServerEvent:Connect(Purchase.Validate)
-
-Players.PlayerAdded:Connect(OnPlayerAdded)
-PlotManager.Init()
-Inventory.Init()
-
-
--- ServerScriptService --> SystemsServer(Folder) --> BuildingSystem(Folder)
-
 local Plots : Folder = game.Workspace:FindFirstChild("Plots")
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local ServerScriptService = game:GetService("ServerScriptService")
 local Players = game:GetService("Players")
-
 local PlotManager = {}
--- Handles all plot management , building and deleting on player's plot ( server sided )
-
+--plots that nobody owns yet
 local available : {Model} = Plots:GetChildren()
+
+--plots currently owned by players (keyed by UserId)
 local ownedPlots : {Model} = {}
+
+--networking
 local Packets = require(ReplicatedStorage.Shared.Packages.Packets)
+
+--object templates players can place
 local objectTemplates = ReplicatedStorage.Shared.ObjectTemplates
+
+--inventory system
 local InventoryServer = require(ServerScriptService.SystemsServer.InventorySystem.InventoryServer)
+
+--shared types
 local Types = require(ReplicatedStorage.Shared.Types)
-local PlacementValidator = require(ReplicatedStorage.Shared.PlacementValidator)
+
+-- config / object data
 local Data = require(ServerScriptService.SystemsServer.Configuration.Data)
 
--- give each player that joins a plot and make it unavailable
+--setup remotes + player events
 function PlotManager.Init()
-	
 	Packets.getPlot.OnServerInvoke = function(player)
 		return PlotManager.GetPlot(player)
 	end
-
-	-- connections
 	Players.PlayerAdded:Connect(PlotManager.OnPlayerAdded)
 	Players.PlayerRemoving:Connect(PlotManager.OnPlayerRemoving)
 	Packets.placeObject.OnServerEvent:Connect(PlotManager.Place)
@@ -67,21 +36,21 @@ function PlotManager.Init()
 	Packets.launch.OnServerEvent:Connect(PlotManager.Launch)
 end
 
-function PlotManager.OnPlayerAdded(player : Player) 
-	-- this runs when player joins 
+-- give player a plot when they join
+function PlotManager.OnPlayerAdded(player : Player)
+	
 	if ownedPlots[player.UserId] or not player:IsA("Player") then
-		return
+		return -- already has plot or invalid
 	end
-
-	-- assign a new plot to new players
-	local newPlot = table.remove(available)
-	newPlot.Name = player.Name.. "'s Plot"
-	player.RespawnLocation = newPlot:FindFirstChild("SpawnLocation")
-	ownedPlots[player.UserId] = newPlot
+	
+	local newPlot = table.remove(available) -- grabs available plots
+	newPlot.Name = player.Name .. "'s Plot" -- rename plot to plr name
+	player.RespawnLocation = newPlot:FindFirstChild("SpawnLocation")-- set plr respawn location
+	ownedPlots[player.UserId] = newPlot	-- mark plot as owned
 	
 	local char = player.Character or player.CharacterAdded:Wait()
-
-	--- assign a function on player's death/respawning event
+	
+	-- handle deaths so launch state stays correct
 	local function onCharacterAdded(char)
 		local humanoid : Humanoid = char:WaitForChild("Humanoid")
 		humanoid.Died:Connect(function()
@@ -90,30 +59,28 @@ function PlotManager.OnPlayerAdded(player : Player)
 	end
 	
 	task.spawn(onCharacterAdded , char)
-	-- this runs instantly with char as arguement
-	
 	player.CharacterAdded:Connect(onCharacterAdded)
 end
 
--- remove plot from player once player leave the game and make it available again
+-- cleanup when player leaves
 function PlotManager.OnPlayerRemoving(player : Player)
-
-	-- sanity check 
 	if not ownedPlots[player.UserId] or not player:IsA("Player") then
 		return
 	end
-
+	
 	if player:GetAttribute("Launched") then
 		PlotManager.Launch(player)
 	end
 	
+	-- making plot available again
 	ownedPlots[player.UserId].Name = "Plot"
 	table.insert(available , ownedPlots[player.UserId])
 	ownedPlots[player.UserId] = nil
 	
 end
 
--- handles player's respawn
+-- called after death if plr launched
+-- to reset models and launch state
 function PlotManager.OnCharacterAdded(player)
 	if not player:GetAttribute("Launched") then
 		return
@@ -139,26 +106,35 @@ function PlotManager.GetPlot(player : Player) : Model
    return ownedPlots[player.UserId]
 end
 
+--placing system
 function PlotManager.Place(player : Player , objectName : string  , objectCF : CFrame )
 	local inventory : Types.inventory = InventoryServer.GetInventory(player)
+	
 	if not inventory or PlotManager.GetStackData(inventory , objectName).Amount == 0 then
 		return
-	end
+	end -- sanity check for if player really has the object
 	
 	local plot : Model? = ownedPlots[player.UserId]
 	local objectTemplate : Model? = objectTemplates[objectName]
+	-- invalid placement
 	if not plot or not objectTemplate or not Data.GetObjectData(objectName) then
 		return
 	end
-	if not PlacementValidator.WithinBounds(plot , objectTemplate:GetExtentsSize() , objectCF) then
+
+	-- outside plot bounds -> stop
+	if not PlotManager.WithinBounds(plot, objectTemplate:GetExtentsSize(), objectCF) then
 		return
 	end
 	
+	--clone and position object
 	local newObject = objectTemplate:Clone()
 	newObject:PivotTo(objectCF)
 	
-	local intersect , overlappingObject = PlacementValidator.IntersectingObject(plot , objectTemplate:GetExtentsSize() , objectCF)
+	local intersect , overlappingObject = PlotManager.Intersecting(plot , objectTemplate:GetExtentsSize() , objectCF)
+	-- check if objects are intersecting and get the object its intersecting with
+	
 	if intersect and overlappingObject then
+		-- if its intersecting weld it together
 		local weld = Instance.new("WeldConstraint")
 		weld.Part0 = newObject.PrimaryPart
 		weld.Part1 = overlappingObject
@@ -167,6 +143,7 @@ function PlotManager.Place(player : Player , objectName : string  , objectCF : C
 	end
 	
 	newObject.Parent = plot.Objects
+	-- remove from inventory
 	InventoryServer.UnregisterObject(player , newObject)
 end
 
@@ -179,18 +156,21 @@ function PlotManager.Delete(player : Player , object : Part)
 	
 	local actualObject = object
 	
-	-- keeps looping until we find the model that is parented to the object folder
+	-- climb up until we hit the model in Objects folder
+	-- (model is parented to the Objects folder)
 	while actualObject.Parent ~= plot.Objects do
 		actualObject = actualObject.Parent
 	end
 	
 	actualObject:Destroy()
 	InventoryServer.RegisterObject(player , actualObject)
+	-- delete from plot and registers it back to the inventory
 end
 
 function PlotManager.Launch(player : Player)
-	local plot : Model? = PlotManager.GetPlot(player)
+	local plot : Model? = ownedPlots[player.UserId]
 	if not plot then
+		warn("Error")
 		return
 	end
 	
@@ -199,23 +179,28 @@ function PlotManager.Launch(player : Player)
 	if not player:GetAttribute("Launched") then
 		player:SetAttribute("Launched" , true)
 		local objectsFolder : Folder = plot.Objects
-
+		
+		-- unanchor everything in the ship ( so it can move )
 		for _ , object : BasePart  in objectsFolder:GetDescendants() do
 			if object:IsA("BasePart") then
 				object.Anchored = false
 			end
 		end
+
 		actualPlot.Color = Color3.fromRGB(0, 0, 255)
 		actualPlot.AssemblyLinearVelocity = actualPlot.CFrame.LookVector * 50
-		
+		print("Launch")
 	else
+		-- setting it back to normal
 		player:SetAttribute("Launched" , false)
 		local actualPlot : Part = plot.Plot
-		actualPlot.Color = Color3.fromRGB(40, 127, 71)y
+		actualPlot.Color = Color3.fromRGB(40, 127, 71)
 		actualPlot.AssemblyLinearVelocity = actualPlot.CFrame.LookVector * 0
+		print("Unlaunch")
 	end
 end
 
+-- get inventory stack for an object
 function PlotManager.GetStackData(inventory : Types.inventory , objectName : string)
 	for _ , stack in inventory.Inventory do
 		if stack.Name == objectName then
@@ -224,283 +209,45 @@ function PlotManager.GetStackData(inventory : Types.inventory , objectName : str
 	end
 end
 
-
-
-
-return PlotManager
-
--- ServerScriptService --> SystemsServer(Folder) --> InventorySystem(Folder)
-local ReplicatedStorage = game:GetService("ReplicatedStorage")
-local Players = game:GetService("Players")
-local Data = require(script.Parent.Parent.Configuration.Data)
-local Types = require(ReplicatedStorage.Shared.Types)
-local Janitor = require(ReplicatedStorage.Shared.Packages.Janitor)
-local Packets = require(ReplicatedStorage.Shared.Packages.Packets)
-local HTTPSERVICE = game:GetService("HttpService")
-local DataStoreService = game:GetService("DataStoreService")
-local InventoryDataStore = DataStoreService:GetDataStore("InventoryDataStore")
-
-local InventoryServer = {}
-
-InventoryServer.AllInventories = {}
-InventoryServer.Janitors = {}
-InventoryServer.HasLoaded = {}
-
-function InventoryServer.Init()
-	
-	Players.PlayerAdded:Connect(InventoryServer.OnPlayerAdded)
-	Players.PlayerRemoving:Connect(InventoryServer.OnPlayerRemoving)
-end
-
-function InventoryServer.OnPlayerAdded(player : Player)
-	-- creates new janitor
-	local newJanitor = Janitor.new()
-	InventoryServer.Janitors[player.UserId] = newJanitor
-	
-	-- creates new inventory
-	local newInventory : Types.inventory = {
-		Inventory = {},	
-	}
-	InventoryServer.AllInventories[player.UserId] = newInventory
-	InventoryServer.LoadData(player)
-	Packets.updateInventoryData.OnServerInvoke = InventoryServer.GetInventory(player)
-	-- cleanup
-	newJanitor:Add(function()
-		InventoryServer.AllInventories[player.UserId] = nil
-		InventoryServer.Janitors[player.UserId] = nil
-	end, true)
-end
-
-
-function InventoryServer.OnPlayerRemoving(player : Player)
-	InventoryServer.SaveData(player)
-	InventoryServer.Janitors[player.UserId]:Cleanup()
-end
-
--- registers new objects to player inventory
-function InventoryServer.RegisterObject(player : Player , object)
-	if not Data.GetObjectData(object.Name) or not InventoryServer.AllInventories[player.UserId] then
-		warn("Error: Invalid object or player")
-		return 
-	end
-	
-	local inventory : Types.inventory = InventoryServer.AllInventories[player.UserId]
-	
-	-- check if there is a stack for the item
-	local foundStack = nil
-	
-	for _ , stack in inventory.Inventory do
-		if stack.Name == object.Name then
-			stack.Amount += 1 
-			foundStack = stack
-			break
+function PlotManager.WithinBounds(plot : Model , objectSize : Vector3 , worldCF : CFrame) : boolean
+	local plotCF , plotSize = plot:GetBoundingBox()
+	local objectCF = plotCF:ToObjectSpace(worldCF)
+	--create corners in X/Z
+	--corner coordinates are generated using:
+	--(-1,  1), ( 1, -1), ( 1,  1), (-1, -1)
+	local cornerpoints = {}
+	for _ , x in pairs({-1, 1}) do
+		for _ , z in pairs({-1 , 1}) do
+			table.insert(cornerpoints , objectCF:PointToWorldSpace(Vector3.new(x * objectSize.X / 2 , 0 , z * objectSize.Z / 2))) 
+			-- Convert the local corner into plot-local space
+			-- then insert each corner in cornerpoints table
 		end
 	end
-	
-	if not foundStack then
-		
-		local newStack : Types.stack = {
-			
-			Name = object.Name,
-			Amount = 1,
-			Image = "rbxassetid://" .. Data.GetObjectData(object.Name).ImageID,
-		}
-		
-		table.insert(inventory.Inventory , newStack)
-	end
-	Packets.updateInventory:FireClient(player , inventory)
-end
 
-function InventoryServer.UnregisterObject(player : Player , object)
-	
-	if not Data.GetObjectData(object.Name) or not Players:GetPlayerByUserId(player.UserId) then
-		return 
-	end
-	
-	local inventory : Types.inventory = InventoryServer.AllInventories[player.UserId]
-	
-	local foundStack = nil
-	
-	for _ , stack in inventory.Inventory do
-		if stack.Name  == object.Name then
-			foundStack = stack
-			break
+	for _ , point : Vector3 in cornerpoints do
+		if math.abs(point.X) > plotSize.X / 2 or math.abs(point.Z) > plotSize.Z / 2 then
+			return false
+			--check each corner point against the plot bounds
+			--If ANY corner is outside the plot, placement is invalid
 		end
 	end
-	
-	if not foundStack or foundStack.Amount == 0 then 
-		return
-	end
-	
-	foundStack.Amount -= 1
-	Packets.updateInventory:FireClient(player , inventory)
+	return true -- ALL corners inside plot so placement is valid
 end
 
-function InventoryServer.SaveData(player : Player , plot)
-	
-	if not InventoryServer.HasLoaded[player.UserId]then 
-		return
-	end
-	
-	local plot = game.Workspace.Plots:FindFirstChild(player.Name.. "'s Plot")
-	
-	if not plot then
-		warn("No plot found")
-		return
-	end
-	
-	for _ , object in plot.Objects:GetChildren() do
-		if object:IsA("Model") then
-			InventoryServer.RegisterObject(player , object)
-			object:Destroy()
-		end
-	end
-	
-	
-	local inv : Types.inventory = InventoryServer.AllInventories[player.UserId]
+function PlotManager.Intersecting(plot : Model , objectSize : Vector3 , worldCF : CFrame) : boolean
+	local params = OverlapParams.new()
+	params:AddToFilter(plot.Objects)-- folder
+	params.FilterType = Enum.RaycastFilterType.Include
+	-- ONLY considers object in Objects folder to be intersecting
 
-	if not inv then
-		return
-	end
+	local overlappingPart : {Instance} = workspace:GetPartBoundsInBox(worldCF , objectSize , params) -- creates a box surrounding object
 
-	
-	local modifiedInv = {
-		Inventory = {}
-	}
-	
-	for _ , stack in inv.Inventory do
-		table.insert(modifiedInv.Inventory , {
-			Name = stack.Name,
-			Amount = stack.Amount,
-			Image = stack.Image,
-		})
-	end
-	
-	local dataString = HTTPSERVICE:JSONEncode(modifiedInv)
-	
-	local success , result = false , nil
-	local TIME_OUT  = 5
-	local currentTime = os.time()
-	
-	while not success do
-		if os.time() - currentTime > TIME_OUT then
-			return
-		end
-		
-		success , result = pcall(function()
-			InventoryDataStore:SetAsync(player.UserId , dataString)
-			print("saved data")
-			if not success then task.wait(1) end
-		end)
-	end
-end
-
-function InventoryServer.LoadData(player : Player)
-	
-	local dataString = InventoryDataStore:GetAsync(player.UserId)
-	if not dataString then
-		InventoryServer.HasLoaded[player.UserId] = true
-		return
-	end
-	
-	local savedData = HTTPSERVICE:JSONDecode(dataString)
-	
-	local inv : Types.inventory = {
-		Inventory = {}
-	}
-
-	for _ , stack in savedData.Inventory do 
-		local objectData = Data.GetObjectData(stack.Name)
-		
-		if not objectData then
-			return
-		end
-		
-		local stack : Types.stack = {
-			Name = stack.Name,
-			Amount = stack.Amount,
-			Image = "rbxassetid://" .. objectData.ImageID,
-		}
-		
-		table.insert(inv.Inventory , stack)
-	end
-	
-	InventoryServer.AllInventories[player.UserId] = inv
-	InventoryServer.HasLoaded[player.UserId] = true
-	print(InventoryServer.HasLoaded[player.UserId])
-	InventoryServer.Janitors[player.UserId]:Add(function()
-		InventoryServer.HasLoaded[player.UserId] = nil
-	end)
-	
-	print("loaded data")
-	
-	Packets.updateInventory:FireClient(player , inv)
-end
-
-function InventoryServer.GetInventory(player : Player) : Types.inventory
-	while not InventoryServer.AllInventories[player.UserId] do
-		task.wait()
-	end
-	return InventoryServer.AllInventories[player.UserId]
-end
-
-return InventoryServer
-
--- ServerScriptService --> SystemsServer(Folder) --> ShopSystem(Folder)
-local ReplicatedStorage = game:GetService("ReplicatedStorage")
-local Players = game:GetService("Players")
-local Data = require(script.Parent.Parent.Configuration.Data)
-local Inventory = require(script.Parent.Parent.InventorySystem.InventoryServer)
-local objTemplates : Folder = ReplicatedStorage.Shared.ObjectTemplates
-
-local Purchase = {}
-
-function Purchase.Validate(player : Player , objectName : string) : boolean
-
-	if not Data.GetObjectData(objectName) or not Players:GetPlayerByUserId(player.UserId) or not objTemplates[objectName] then
-		return
-	end
-	
-	local money : IntValue = player:FindFirstChild("Money")
-	local object = objTemplates[objectName]  
-	local price = Data.GetObjectData(objectName).Price
-	
-	if money.Value >= price then
-		money.Value -= price
-		Inventory.RegisterObject(player , object)
-		return true
+	if #overlappingPart > 0 then -- if another object is inside the box then its intersecting
+		return true , overlappingPart[1]
 	else
 		return false
 	end
 end
 
-return Purchase
-
--- ServerScriptService --> SystemsServer(Folder) --> Configurations
-local Data = {}
-
-Data.Objects = {
-	
-	Brick = {
-		ImageID = 119927858691427,
-		Price = 100
-	},
-	
-	Wood = {
-		ImageID = 102419410921260,
-		Price = 50
-	},
-	
-	Ice = {
-		ImageID = 128689214655358,
-		Price = 20
-	}	
-}
-
-function Data.GetObjectData(objectName : string)
-	return Data.Objects[objectName]
-end
-
-return Data
-
+return PlotManager
 
